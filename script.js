e0bfccfa9b0cb3ac94c434cc8cbddcf73b36a57f
Marc Byndas <byndasma@gmail.com>
2024-03-08 15:56:43 -0500

    PART TWO written, now debugging

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-const helpers = {
-  addDestinationInfo,
-  validateInput,
-  formSubmission,
-  pickPlanet,
-  myFetch
-} = require("./scriptHelper.js");
-
-// import {
-//   addDestinationInfo,
-//   validateInput,
-//   formSubmission,
-//   pickPlanet,
-//   myFetch
-// } from "./scriptHelper.js";
-
+// const { myFetch, addDestinationInfo, validateInput, formSubmission, pickPlanet } = require("./scriptHelper");
 
 window.addEventListener("load", function () {
-  // const document = document;
-  // const list = document.getElementById("faultyItems");
-  // const pilotName = document.getElementById("pilotStatus");
-  // const copilotName = document.getElementById("copilotStatus");
-  // const fuelLevel = document.getElementById("fuelStatus");
-  // const cargoMass = document.getElementById("cargoStatus");
-  // const h2 = document.getElementById("launchStatus");
-  let listedPlanets;
-  let listedPlanetsResponse = myFetch(); // returns a promise that returns json
+  let list; // = document.getElementById("faultyItems").value;
+  let pilotName = document.getElementById("pilotName").value;
+  let copilotName = document.getElementById("copilotName").value;
+  let fuelLevel = document.getElementById("fuelLevel").value;
+  let cargoMass = document.getElementById("cargoMass").value;
 
-  const form = document.querySelector("form");
+  // let listedPlanets;
+  // let listedPlanetsResponse = myFetch(); // returns a promise that returns planets json
+//  ..........................................................................................................................
+//  ..........................................................................................................................
 
 //  PART ONE: VALIDATE INPUTS
 //  .........................
@@ -36,22 +20,37 @@ window.addEventListener("load", function () {
 //          indicate when launch ready
 //  ..................................
 
-  form.addEventListener("submit", helpers.formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass));
-
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass)
+  });
+  
+  //  ..........................................................................................................................
+  //  ..........................................................................................................................
+  
   //  PART TWO: FETCH PLANETARY DATA
   //  ..............................
-  listedPlanetsResponse
-    .then(function (result) {
-      listedPlanets = result;
-      console.log(listedPlanets);
-    })
-    .then(function () {
-      //  open browser dev tools to see planet list
-      console.log(listedPlanets);
+console.log("myFetch", myFetch().stringify());
 
-      //  use pickPlanet() & addDestinationInfo() helper functions
-      //    to select random planet from listedPlanets --> six planets
+  myFetch()
+    .then((result) => {
+      console.log("result", result);
+      let listedPlanets = result;
+      console.log("listedPlanets1", listedPlanets);
+    })
+    .then((listedPlanets) => {
+      // try-catch? --> predicts & bridges errors to continue code flow
+      //  open browser dev tools to see planet list
+      console.log("listedPlanets2", listedPlanets);
+
+      // call pickPlanet() & addDestinationInfo()
+      //    to select a random planet from listedPlanets
       //      & pass that planet info to addDestinationInfo()
       //        reload page to see mission target info
+      let planetDestination = pickPlanet(listedPlanets);
+
+      console.log("planetDestination", planetDestination)
+      // addDestinationInfo(document, ...planetDestination);
+      // name,diameter,star,distance,moons,imageUrl
     });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -22,9 +22,7 @@ function validateInput(testInput) {
   //  fuelLevel & cargoLevel are numbers
   //  NOTE: isNaN(value) method returns true or false
 
-  //  1.  input validation
-  if (testInput.length === 0) {
-    SubmitEvent.preventDefault();
+  if (!testInput.length) {
     alert("Must enter a value for each field.");
     return "Empty";
   }
@@ -40,51 +38,48 @@ function validateInput(testInput) {
 function formSubmission(
   // PARAMETERS:
   document, // D.O.M. --> unnecessary
-  list, // array of remaining parameters or <li> list-items ? --> list = document.querySelector("#---") ?
+  list, // document.querySelector("#launchStatus") ?
   pilot, // text in string
   copilot, // text in string
   fuelLevel, // number in string
-  cargoLevel // number in string
+  cargoLevel, // number in string
+  event
   ) {
-  const launchStatusId = document.querySelector("#launchStatus");
-  //  2.  input type error handling
-  if (validateInput(pilot) === "Is a Number") {
-    SubmitEvent.preventDefault();
+  let launchStatusId = document.querySelector("#launchStatus");
+  //  input type error handling
+  if (validateInput(pilot, event) === "Is a Number") {
     return alert("Must not enter a number.");
   };
-  if (validateInput(copilot) === "Is a Number") {
-    SubmitEvent.preventDefault();
+  if (validateInput(copilot, event) === "Is a Number") {
     return alert("Must not enter a number.");
   };
-  if (validateInput(fuelLevel) === "Not a Number") {
-    SubmitEvent.preventDefault();
+  if (validateInput(fuelLevel, event) === "Not a Number") {
     return alert("Must enter a number.");
   };
-  if (validateInput(cargoLevel) === "Not a Number") {
-    SubmitEvent.preventDefault();
+  if (validateInput(cargoLevel, event) === "Not a Number") {
     return alert("Must enter a number.");
   };
-  //  3.  update launch checklist to display launch-ready status of pilot, copilot, fuel & cargo
-  //    A.  make #faultyItems visible
+  //  update launch checklist to display launch-ready status of pilot, copilot, fuel & cargo
+  //    make #faultyItems visible
   document.querySelector("#faultyItems").style.visibility = "visible";
-  //    B.  use template literals to update list-items
+  //    use template literals to update list-items
   document.querySelector("#pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
   document.querySelector("#copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
-  //    C.  handle insufficient fuel & cargo levels
+  //    handle insufficient fuel & cargo levels
   if (fuelLevel < 10000) {
     launchStatusId.innerHTML = "Shuttle Not Ready for Launch";
     launchStatusId.stlye.color = "red";
     document.querySelector("#fuelStatus").innerHTML = "Fuel level too low for launch";
     return alert("Fuel level too low for launch");
   }
-  if (cargoMass > 10000) {
+  if (cargoLevel > 10000) {
     launchStatusId.innerHTML = "Shuttle Not Ready for Launch";
     launchStatusId.style.color = "red";
     document.querySelector("#cargoStatus").innerHTML = "Cargo mass too heavy for launch";
     return alert("Cargo mass too heavy for launch");
   }
   launchStatusId.innerHTML = "Shuttle is ready for launch";
-  launchStatusId.syle.color = "green";
+  launchStatusId.style.color = "green";
 }
 /*
 ..........................................................................................................................
@@ -101,27 +96,25 @@ fetch planetary JSON data:
   to fetch planetList data:
 
     1.  complete myFetch() to fetch planetary JSON data:
-            add the URL
-            return response.json()
+            add URL & return response.json()
 
     2.  pickPlanet() takes a planetList argument
             use Math.random() to return random index planet from planetList argument
 
     3.  addDestinationInfo() comments show innerHTML format for <div id="missionTarget">,
-            save that element using addDestinationInfo()'s document parameter
-            addDestinationInfo() needs no return value
-....................................................*/
+            save that element, no return value
+............................................*/
 
 async function myFetch() {
-  // try-catch? --> predicts & bridges errors to continue code flow
-  let planetsReturned;
-
-  planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {});
-
-  return planetsReturned;
+  await fetch("https://handlers.education.launchcode.org/static/planets.json").then(response => response.json());
 }
 
-function pickPlanet(planets) {}
+function pickPlanet(planets) {
+  let randomIndex = Math.floor(Math.random() * planets.length);
+
+  console.log("planets[randomIndex]:", planets[randomIndex])
+  return planets[randomIndex];
+}
 
 function addDestinationInfo(
   document,
@@ -132,17 +125,17 @@ function addDestinationInfo(
   moons,
   imageUrl
 ) {
-  /*  add within #missionTarget:
-
-        <h2>Mission Destination</h2>
-        <ol>
-            <li>Name: </li>
-            <li>Diameter: </li>
-            <li>Star: ${star}</li>
-            <li>Distance from Earth: </li>
-            <li>Number of Moons: </li>
-        </ol>
-        <img src="">  */
+  document.querySelector("#missionTarget").innerHTML =
+    `<h2>Mission Destination</h2>
+      <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+    </ol>
+    <img src=${imageUrl}>
+    `;
 }
 
 //  ...................................................
